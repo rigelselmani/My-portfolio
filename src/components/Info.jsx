0966c74954d53099db09512bee85ca37145c33f5
rigel selmani <rigelrigelselmani@hotmail.com>
2020-09-17 21:48:44 -0400

    info file and coding h3, h4 p and technical skills and styling the newwly created

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Info(){
     return <div>
-              <div>
+              <div class="first-section">
                 <h3>Rigel Selmani</h3>
-                <h4>Web Developer</h4>
-                <p>Front-end focused full stack web developer</p>
+                <h4 class="web">Web Developer</h4>
+                <p class="focus">Front-end focused full stack web developer</p>
+                <h4 class="technical">TECHNICAL SKILLS</h4>
+                <p class="skills-list">HTML, CSS, Bootstrap, Media Queries, JavaScript, JQuery,  APIs, JSON, REST, AJAX, Node Js, Express, React.js, GitHub, MongoDB, MySQL, Handlebars, Sequelize, Firebase, mailchimp, the command line, computer science fundamentals.</p>
               </div>
             </div>
 }
