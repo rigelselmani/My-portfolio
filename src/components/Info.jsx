7375aa3672cca217c74118075895c83e6d09bfc9
rigel selmani <rigelrigelselmani@hotmail.com>
2020-09-17 21:39:22 -0400

    Info and App file

---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+    return <div>
+              <div>
+                <h3>Rigel Selmani</h3>
+                <h4>Web Developer</h4>
+                <p>Front-end focused full stack web developer</p>
+              </div>
+            </div>
+}
+
+export default Info;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from "./components/App";
 
 ReactDOM.render(
   <div>
-    <h1>hello World</h1>
+    <App />
   </div>,
   document.getElementById('root')
 );
