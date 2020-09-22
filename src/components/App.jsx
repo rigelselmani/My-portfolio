4c5a3fe0a051e002eae8bafe826a0b0c2cfc75c9
rigel selmani <rigelrigelselmani@hotmail.com>
2020-09-22 01:20:14 -0400

    styling the projects img

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Info from "./Info";
-import Contact from "./Contact";
 import Projects from "./projects"
 
 function App(){
     return  <div>
                 <section className="first-section">
                    <Info />
-                   <Contact />
                 </section>
-                    <Projects />
+                   <Projects />
             </div>
 }
 
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
 function Info(){
-    return    <div>
+    return<div>
                 <h3>Rigel Selmani</h3>
                 <h4 className="web">Web Developer</h4>
                 <p className="focus">Front-end focused full stack web developer</p>
                 <h4 className="technical">TECHNICAL SKILLS</h4>
                 <p className="skills-list">HTML, CSS, Bootstrap, Media Queries, JavaScript, JQuery,  APIs, JSON, REST, AJAX, Node Js, Express, React.js, GitHub, MongoDB, MySQL, Handlebars, Sequelize, Firebase, mailchimp, the command line, computer science fundamentals.</p>
-              </div>
+            <div id="contact">
+              <h4 className="contact">Contact</h4>
+              <a className="number" href="tel:703-870-6927">Phone: +1 703-870-6927</a>
+            
+              <a href="https://www.linkedin.com/in/rigel-selmani-504705177/" target="_blank"><img className="img-contact" src={require('../images/linkedin.jpg')} /></a>
+              
+              <a href="https://github.com/rigelselmani" target="_blank"><img className="img-contact" src={require('../images/github.jpg')} /></a>
+              
+              <a href="mailto:rigelrigelselmani@hotmail.com" target="_blank"><img className="img-contact" src={require('../images/email.jpg')} /></a>
+                 
+              <a href="https://drive.google.com/file/d/1X8oPIKDtYQ8k8Z219SmFaa7uRPbm1vEg/view?usp=sharing" target="_blank"><img className="img-contact" src={require('../images/resume.png')} /></a>
+            </div>
+          </div>
 }
 
 export default Info;
