d94bcd756c095e36584cac0539163d580315574f
rigel selmani <rigelrigelselmani@hotmail.com>
2020-09-21 23:57:53 -0400

    adding projects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Info from "./Info";
-import Contact from "./Contact"
+import Contact from "./Contact";
+import Projects from "./projects"
 
 function App(){
     return <section className="first-section">
                <Info />
                <Contact />
+               <Projects />
            </section>
 }
 
---
+++ src/components/projects.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function projects(){
+    return <div class="second-section">
+      <h2 class="second-head">Projects</h2>
+      <div class="work">
+        <a href="https://rigelselmani.github.io/WEATHER/" target="_blank"><img src="https://i1.wp.com/www.learnesl.net/wp-content/uploads/2018/03/BigStock-Weather-Forecast-1-678x381.jpg" /></a>
+      </div>
+      <div class="work">
+        <a href="https://rigelselmani.github.io/Word-Scape/" target="_blank"><img src="assets/images/logo.png" /></a>
+      </div>
+      <div class="work">
+        <a href="https://movie-search-react1.herokuapp.com/" target="_blank"><img src="assets/images/movie-search.png" /></a>
+      </div>
+      <div class="work">
+        <a href="https://rigelselmani.github.io/To-Do-List/" target="_blank"><img src="assets/images/17-stress-free-errands.jpg" /></a>
+      </div>
+      <div class="work">
+        <a href="https://rigelselmani.github.io/TriviaGame/" target="_blank"><img src="assets/images/IQT-Widescreen-Banner.jpg" /></a>
+      </div>
+      <div class="work">
+        <a href="https://secret-dawn-47186.herokuapp.com/" target="_blank"><img src="assets/images/AdultFriendFinder-Logo.jpg" /></a>
+      </div>
+    </div>
+    
+
+}
+
+export default projects;
