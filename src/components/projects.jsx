b6b702f427ef14a88add64b5dbb012dbf7b5d6aa
rigel selmani <rigelrigelselmani@hotmail.com>
2020-10-20 02:03:50 -0400

    changes in the security of the project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,22 +4,22 @@ function projects(){
     return <section className="second-section">
       <h2 className="second-head">Projects</h2>
       <div className="work">
-        <a href="https://rigelselmani.github.io/WEATHER/" target="_blank"><img src="https://i1.wp.com/www.learnesl.net/wp-content/uploads/2018/03/BigStock-Weather-Forecast-1-678x381.jpg" /></a>
+        <a href="https://rigelselmani.github.io/WEATHER/" target="_blank" rel="noopener noreferrer"><img src="https://i1.wp.com/www.learnesl.net/wp-content/uploads/2018/03/BigStock-Weather-Forecast-1-678x381.jpg" alt="display" /></a>
       </div>
       <div className="work">
-        <a href="https://murmuring-springs-09719.herokuapp.com/" target="_blank"><img src={require('../images/logo.png')} /></a>
+        <a href="https://murmuring-springs-09719.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={require('../images/logo.png')} alt="display" /></a>
       </div>
       <div className="work">
-        <a href="https://movie-search-react1.herokuapp.com/" target="_blank"><img src={require('../images/movie-search.png')} /></a>
+        <a href="https://movie-search-react1.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={require('../images/movie-search.png')} alt="display" /></a>
       </div>
       <div className="work">
-        <a href="https://rigelselmani.github.io/To-Do-List/" target="_blank"><img src={require('../images/to-do.jpg')} /></a>
+        <a href="https://rigelselmani.github.io/To-Do-List/" target="_blank" rel="noopener noreferrer"><img src={require('../images/to-do.jpg')} alt="display" /></a>
       </div>
       <div className="work">
-        <a href="https://glacial-eyrie-21309.herokuapp.com/" target="_blank"><img src={require('../images/note.jpg')} /></a>
+        <a href="https://glacial-eyrie-21309.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={require('../images/note.jpg')} alt="display" /></a>
       </div>
       <div className="work">
-        <a href="https://secret-dawn-47186.herokuapp.com/" target="_blank"><img src={require('../images/friend.jpg')} /></a>
+        <a href="https://secret-dawn-47186.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={require('../images/friend.jpg')} alt="display" /></a>
       </div>
     </section>
     
