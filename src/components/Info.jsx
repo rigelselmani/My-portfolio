3c90e45b8184fb037bae387f9c2841a34a024d4d
rigel selmani <rigelrigelselmani@hotmail.com>
2020-12-07 19:47:49 -0500

    changing the contact style and using icons instead of pic

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,19 +7,32 @@ function Info(){
                 <p className="focus">Front-end focused full stack web developer</p>
                 <h4 className="technical">TECHNICAL SKILLS</h4>
                 <p className="skills-list">HTML, CSS, Bootstrap, Media Queries, JavaScript, JQuery,  APIs, JSON, REST, AJAX, Node Js, Express, React.js, GitHub, MongoDB, MySQL, Handlebars, Sequelize, Firebase, mailchimp, the command line, computer science fundamentals.</p>
+          <div className="info-section">  
             <div id="contact">
-              <h4 className="contact">Contact</h4>
-              <a className="number" href="tel:703-870-6927">Phone: +1 703-870-6927</a>
-            
-              <a href="https://www.linkedin.com/in/rigel-selmani-504705177/" target="_blank" rel="noopener noreferrer"><img className="img-contact" src={require('../images/linkedin.jpg')} alt="contact" /></a>
-              
-              <a href="https://github.com/rigelselmani" target="_blank" rel="noopener noreferrer"><img className="img-contact" src={require('../images/github.jpg')} alt="contact" /></a>
-              
-              <a href="mailto:rigelrigelselmani@hotmail.com" target="_blank" rel="noopener noreferrer"><img className="img-contact" src={require('../images/email.jpg')} alt="contact" /></a>
-                 
-              <a href="https://drive.google.com/file/d/1X8oPIKDtYQ8k8Z219SmFaa7uRPbm1vEg/view?usp=sharing" target="_blank" rel="noopener noreferrer"><img className="img-contact" src={require('../images/resume.png')} alt="contact" /></a>
+              <h6 className="contact">Links and Contact Info</h6>
+              <div className="contact-text">
+                <a href="mailto:rigelrigelselmani@hotmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+                <p className="contact-p">email</p>
+              </div>
+              <div className="contact-text">
+                <a href="tel:703-870-6927" rel="noopener noreferrer"><i class="fas fa-phone-alt"></i></a>
+                <p className="contact-p">phone</p>
+              </div>
+              <div className="contact-text">
+                <a href="https://drive.google.com/file/d/1X8oPIKDtYQ8k8Z219SmFaa7uRPbm1vEg/view?usp=sharing" target="_blank" rel="noopener noreferrer"><i class="far fa-file"></i></a>
+                <p className="contact-p">ressume</p>
+              </div>
+              <div className="contact-text">
+                <a href="https://www.linkedin.com/in/rigel-selmani-504705177/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+                <p className="contact-p">linkedin</p>
+              </div>
+              <div className="contact-text">
+                <a href="https://github.com/rigelselmani" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+                <p className="contact-p">github</p>
+              </div> 
             </div>
           </div>
+        </div>
 }
 
 export default Info;
